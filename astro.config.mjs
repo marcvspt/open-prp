59bfd6bd01bd48ea48839f55100b712d8a56f979
marcvspt <78035694+marcvspt@users.noreply.github.com>
2026-07-19 17:03:41 -0600

    Primera version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+
+import react from '@astrojs/react';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  site: 'https://phrp.marcvspt.tech',
+
+  env: {
+    schema: {
+      TURSO_BD_URL: envField.string({
+        context: 'server',
+        access: 'secret',
+        optional: false
+      }),
+      TURSO_DB_TOKEN: envField.string({
+        context: 'server',
+        access: 'secret',
+        optional: false
+      }),
+    }
+  }
+});
